ed7db3bc59823a360ba612b375b9c38bc2a018a5
valer-sky <89993618+valer-sky@users.noreply.github.com>
2021-10-26 21:03:41 +0300

    e

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 //Практика
 const numberOfFilms = prompt('Сколько фильмов вы посмотрели?');
-const lastFilms = prompt('Один из последних просмотренных фильмов?')
-const raiting  = prompt('На сколько оцените его?')
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {
-        [lastFilms]: raiting 
-    },
+    count: 0,
+    movies: [],
     actors:{},
     genres: [],
     privat: false
 };
+if (numberOfFilms > 0) {
+    for(let i = 1; i <= numberOfFilms; i++){
+        let lastFilms = prompt('Один из последних просмотренных фильмов?');
+        
+        while (!lastFilms || lastFilms.length === 0){
+            lastFilms = prompt('Один из последних просмотренных фильмов?');
+        }
+        let raiting  = +prompt('На сколько оцените его? От 1 до 5');
+        while(!(raiting >= 1 && raiting <=5)){
+        raiting = +prompt('На сколько оцените его? От 1 до 5')
+        }
+        personalMovieDB.movies.push({[lastFilms]: raiting})
+            
+    }
+} else alert ('Вы ввели некоректное значение');
+if (personalMovieDB.count < 10){
+    alert('Посмотрено довольно мало фильмов');
+
+}else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    alert('Вы классический зритель')
+
+}else if (personalMovieDB.count > 30){
+    alert('Вы киноман')
+}else alert('Произошла ошибка')
+
+
+
 console.log(personalMovieDB);
+
 
