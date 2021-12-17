4dffab205c63bb7653ff60e3c6fc8e93f09a4a6c
valer-sky <89993618+valer-sky@users.noreply.github.com>
2021-12-17 12:06:27 +0300

    Timer

--- it-academy-food-js/it-academy-food-js/Food/js/script.js
+++ it-academy-food-js/it-academy-food-js/Food/js/script.js
@@ -36,11 +36,69 @@
       }
     });
     //
-    const timerId = setTimeout(function() {
-console.log('Hello');
-    }, 2000);
+//     const timerId = setTimeout(function() {
+// console.log('Hello');
+//     }, 2000);
 
-    
+    //ТАЙМЕР
+
+    const deadline = '2022-01-01';
+
+    function getTimeRemaining(endtime) {      // математически округляем числа
+      const t = Date.parse(endtime) - new Date(),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+      
+      return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+      };
+    }
+      function getZero(num) {       // вставляем ноль перед днем и часом когда меньше 10
+        if (num >= 0 && num < 10) {
+          return `0${num}`;
+        } else {
+          return num;
+        }
+      }
+
+      function setClock(selector, endtime) {     // обращаемся к классам на страничке(спаны)
+      const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+      updateClock();  // исключает маргании при обновлении страницы
+      
+      function updateClock() {
+        const t = getTimeRemaining(endtime);
+
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+
+        if (t.total <= 0) {     // отключаем таймер когда дедлайн
+          clearInterval(timeInterval);
+          days.innerHTML = 0;
+          hours.innerHTML = 0;
+          minutes.innerHTML = 0;
+          seconds.innerHTML = 0;
+        }
+
+      }
+
+    }
+
+    setClock('.timer', deadline); //запускаем таймер функцией
+// 
 
  });
 // window.addEventListener('DOMContentLoaded', function() {
